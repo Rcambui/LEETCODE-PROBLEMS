f76adc88b17cb8461358d3ee46fe30a19eb7bfc8
Ricardo Cambui <r.cambui@hotmail.com>
2023-03-12 12:01:38 -0300

    Problem solved

---
+++ src/LeetCode485_MaxConsecutiveOnes.java
@@ -0,0 +1,44 @@
+
+/*Given a binary array nums, return the maximum number of consecutive 1's 
+in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 
+1s. The maximum number of consecutive 1s is 3.
+Example 2:
+
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.*/
+
+public class LeetCode485_MaxConsecutiveOnes {
+    
+    public int findMaxConsecutiveOnes(int[] nums) {
+        
+        int consecutive = 0;
+        int inARow = 0;
+        
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] == 1) {
+                consecutive++;
+                if (inARow < consecutive) {
+                    inARow = consecutive;
+                }
+            }
+            else {
+                consecutive = 0;
+            }
+        }
+        return inARow;
+    }
+}
